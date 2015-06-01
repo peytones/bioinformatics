8ea077e1440da00d321215aa4d6fdb013a32d257
peytones <peytonsarmiento@gmail.com>
2015-05-31 23:16:03 -0500

    Removed files that are supposed to only be in the bio folder

--- expoffspring.py
+++
@@ -1,7 +0,0 @@
-f = open('rosalind_iev.txt','r')
-probabilites = {1:1, 2:1, 3:1, 4:.75, 5:.5, 6:0}
-parents = [int(x) for x in f.readline().split()]
-offspring = 0
-for i in xrange(1,6,1):
-	offspring += int(parents[i])*probabilites[i]*2
-print offspring
--- indpalleles.py
+++
@@ -1,6 +0,0 @@
-k = 2
-n = 1
-prob = 0
-for i in xrange(1,k+1,1):
-	prob += 1.0/(4 ** (i)) *  (2 ** i)
-print prob
