7d2e9dd6572f75cca45b94b6cff2dd2ca63ac948
peytones <peytonsarmiento@gmail.com>
2015-06-07 22:22:52 -0500

    updated kmers lexigraphically, still work in progress

--- bio/kmerslexi.py
+++ bio/kmerslexi.py
@@ -3,4 +3,4 @@ symbols = f.readline().strip('\n').replace(' ', '')
 length = int(f.readline().strip('\n'))
 for i in range(len(symbols)):
 	for k in range(len(symbols)):
-		print symbols[i]+symbols[k:k+length-1]
+		print symbols[i]+symbols[k:k+length]
