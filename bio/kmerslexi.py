e4bf790914edecf4e4035e19c220eca821f8c753
peytones <peytonsarmiento@gmail.com>
2015-06-09 22:28:31 -0500

    solution to finding kmers of a string lexigraphically

--- bio/kmerslexi.py
+++ bio/kmerslexi.py
@@ -1,6 +1,12 @@
-f = open('kmer.txt','r')
+f = open('rosalind_lexf.txt','r')
 symbols = f.readline().strip('\n').replace(' ', '')
 length = int(f.readline().strip('\n'))
 for i in range(len(symbols)):
 	for k in range(len(symbols)):
-		print symbols[i]+symbols[k:k+length]
+		for j in range(len(symbols)):
+			for l in range(len(symbols)):
+				length += 0
+
+#much better solution. product acts as nested for loop that I created by hand in my solution
+from itertools import product
+print '\n'.join([''.join(x) for x in product(symbols,repeat=length)])
